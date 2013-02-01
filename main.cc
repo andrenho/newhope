9a86a0850c043bae1d1c7263f72fbcc845247017
André Wagner <andre_nho@gmail.com>
2013-01-31 22:04:08 -0200

    Added new null library (wip).

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include "libs/sdl/sdl.h"
+#include "libs/null/nulllib.h"
 #include "ui/ui.h"
 #include "util/i18n.h"
 #include "util/logger.h"
@@ -11,7 +12,7 @@ int main(int argc, char** argv)
 	// initialization
 	i18n_init();
 	World world(5000, 5000);
-	GraphicLibrary* video(new SDL());
+	GraphicLibrary* video(new NullLibrary());
 	UI* ui(new UI(world, *video));
 
 	// main loop
