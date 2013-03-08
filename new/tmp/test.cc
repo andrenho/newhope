d7ae276f761c1943a563d5ffcb706cf70f560961
André Wagner <andre_nho@gmail.com>
2013-03-08 16:58:38 -0300

    Tests.

--- new/tmp/test.cc
+++ new/tmp/test.cc
@@ -9,15 +9,20 @@ using namespace std;
 
 int main()
 {
+	//
+	// initialize
+	//
 	if(!glfwInit()) {
 		cerr << "Failed to initialize GLFW\n" << endl;
 		return -1;
 	}
 
+	//
+	// open window
+	//
 	glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 2);
 	glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 1);
-	//glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	if(!glfwOpenWindow(200, 200, 0, 0, 0, 0, 32, 0, GLFW_WINDOW)) {
 		cerr << "Failed to open GLFW window\n" << endl;
@@ -25,16 +30,48 @@ int main()
 		return -1;
 	}
 
-	glewExperimental = true;
+	// 
+	// initialize glew
+	//
 	if(glewInit() != GLEW_OK) {
 		cerr << "Failed to initialize GLEW\n" << endl;
 		return -1;
 	}
-	glfwSetWindowTitle("test");
 
+	glfwSetWindowTitle("test");
 	glfwEnable(GLFW_STICKY_KEYS);
 
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	// 
+	// create VAO
+	//
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	static const GLfloat g_vertex_buffer_data[] = { 
+                -1.0f, -1.0f, 0.0f,
+                 1.0f, -1.0f, 0.0f,
+                 0.0f,  1.0f, 0.0f,
+        };
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
+			g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	//
+	// events
+	//
 	do {
+		// draw
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw triangle
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
+
+		// swap buffers
 		glfwSwapBuffers();
 	} while(glfwGetKey(GLFW_KEY_ESC) != GLFW_PRESS && glfwGetWindowParam(GLFW_OPENED));
 }
