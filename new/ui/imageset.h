c2d34eee10ce8d89caacb8bceb7ada46282a575f
André Wagner <andre_nho@gmail.com>
2013-03-10 14:44:06 -0300

    Changes.

--- new/ui/imageset.h
+++ new/ui/imageset.h
@@ -10,13 +10,17 @@ typedef struct Reference {
 class Imageset {
 public:
 	Imageset();
+	~Imageset();
 
 	const Reference operator[](std::string s) const { return hash_.at(s); }
 	unsigned int const* Texture() const { return texture_; }
 
 private:
+	void LoadImage(string filename);
+
 	std::map<std::string, Reference> hash_;
-	unsigned int texture_[1];
+	unsigned int* texture_;
+	int n_textures, c_texture;
 };
 
 #endif
