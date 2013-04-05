d3101f318c9f985ec0eaf4f923099a9577322dec
André Wagner <andre_nho@gmail.com>
2013-04-05 11:21:37 -0300

    Adding trees.

--- new/ui/layerobject.cc
+++ new/ui/layerobject.cc
@@ -3,4 +3,51 @@
 void 
 LayerObject::Render() const
 {
+	ui->Scene().setProportion(PROP_TILES);
+	
+	int x1, y1, x2, y2;
+	ui->Scene().WindowTileBorders(x1, y1, x2, y2);
+	
+	multimap<Point<float>, Object> objects;
+	CreateObjectList(objects, x1, y1, x2, y2);
+
+	vector<Point<float>> pos;
+	SortObjectPoints(objects, pos);
+
+	for(auto const& p: pos) {
+		auto obj_it = objects.equal_range(p);
+		for(auto obj=obj_it.first; obj != obj_it.second; ++obj) {
+			DrawObject(obj->second);
+		}
+	}
+}
+
+
+void 
+LayerObject::CreateObjectList(multimap<Point<float>, Object>& objects,
+		int x1, int y1, int x2, int y2) const
+{
+	for(int x=x1; x<=x2; x++) {
+		for(int y=y1; y<=y2; y++) {
+			TreeType tt = game->Map().Tree(Point<int>(x, y));
+			if(tt != tt_NONE) {
+				objects.insert(pair<Point<float>, Object>(
+							Point<float>(x, y),
+							TreeObj(tt)));
+			}
+		} 
+	}
+}
+
+
+void
+LayerObject::SortObjectPoints(multimap<Point<float>, Object>& objects,
+		              vector<Point<float>> pos) const
+{
+}
+
+
+void
+LayerObject::DrawObject(Object obj) const
+{
 }
--- new/ui/layerobject.h
+++ new/ui/layerobject.h
@@ -3,8 +3,31 @@
 
 #include "defines.h"
 
+#include <map>
+
 class LayerObject : public Layer {
+public:
 	void Render() const;
+
+private:
+	struct Object { 
+		enum ObjType { TREE } type;
+		Object(enum ObjType t) : type(t) { }
+	};
+
+	void CreateObjectList(multimap<Point<float>, Object>& objects, 
+			int x1, int y1, int x2, int y2) const;
+	void SortObjectPoints(multimap<Point<float>, Object>& objects,
+		              vector<Point<float>> pos) const;
+	void DrawObject(Object obj) const;
+
+	//
+	// object types
+	//
+	struct TreeObj : public Object {
+		TreeObj(TreeType ttype) : Object(TREE), ttype(ttype) { }
+		TreeType ttype;
+	};
 };
 
 #endif
