b6b412e7f7b71255e45b12bdccd1270e28e95c36
André Wagner <andre.nho@gmail.com>
2014-01-01 22:17:23 -0200

    .

--- engine/mapgen.lua
+++ engine/mapgen.lua
@@ -4,7 +4,7 @@ MapGen.__index = MapGen
 function MapGen:new(x1, y1, x2, y2)
    local self = setmetatable({}, MapGen)
    self.__x1, self.__y1, self.__x2, self.__y2 = x1, y1, x2, y2
-   self.__outer_polygon = {}
+   self.__outer_rectangle = {}
    self.polygon_biome = {}
    return self
 end
@@ -18,7 +18,7 @@ end
 
 function MapGen:tile(x,y)
    for i,poly in ipairs(self.voronoi.polygons) do
-      if self:__polycontains(poly, x, y) then
+      if self:__polycontains(i, x, y) then
          return self.polygon_biome[i]
       end
    end
@@ -31,10 +31,24 @@ end
 -------------
 
 function MapGen:__create_polygons()
-   local vor = voronoi:new(200, 3, self.__x1, self.__y1, 
+   -- create voronoi diagram
+   local vor = voronoi:new(500, 2, self.__x1, self.__y1, 
                              -self.__x1+self.__x2, -self.__y1+self.__y2)
-   --for i,poly in ipairs(vor.polygons) do
-   --   self.__outer_polygon[i] = 
+   -- find outer rectangles
+   for i,poly in ipairs(vor.polygons) do
+      local points_x, points_y = {}, {}
+      for j=1,#poly.points,2 do
+         points_x[#points_x+1] = poly.points[j]
+         points_y[#points_y+1] = poly.points[j+1]
+      end
+      self.__outer_rectangle[i] = {
+         x1 = funct.min(points_x),
+         y1 = funct.min(points_y),
+         x2 = funct.max(points_x),
+         y2 = funct.max(points_y),
+      }
+   end
+
    return vor
 end
 
@@ -48,9 +62,13 @@ function MapGen:__create_biomes()
 end
 
 
-function MapGen:__polycontains(poly, x, y)
-   -- TODO - this is too slow, try to make it faster
-   return poly:containspoint(x,y)
+function MapGen:__polycontains(n_poly, x, y)
+   -- TODO - this can be even faster if we create inner rectangles
+   local r = self.__outer_rectangle[n_poly]
+   if x >= r.x1 and x <= r.x2 and y >= r.y1 and y <= r.y2 then
+      return self.voronoi.polygons[n_poly]:containspoint(x,y)
+   end
+   return false
 end
 
 
