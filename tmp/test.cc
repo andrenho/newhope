871f2943b7436aebe32916ccdd5f24eeca65ecfd
André Wagner <andre.nho@gmail.com>
2014-03-05 21:32:33 -0300

    .

--- tmp/test.cc
+++ tmp/test.cc
@@ -9,7 +9,6 @@
 int main()
 {
 	// initalize GLFW
-	glewExperimental = GL_TRUE;
 	glfwInit();
 
 	// create hints for OpenGL
@@ -24,13 +23,79 @@ int main()
 	// make OpenGL context active
 	glfwMakeContextCurrent(window);
 
-	// 
+	// GLEW
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// create triangle vertices
+	GLfloat vertices[] = {
+		 0.0f,  0.5f,
+		 0.5f, -0.5f,
+		-0.5f, -0.5f,
+	};
+
+	// create VAO
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	// initalize VBO
 	GLuint vbo;
 	glGenBuffers(1, &vbo);
 
+	// make VBO the active object
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	// vertex shader
+	const char* vsource = R"VSH(
+#version 150
+
+in vec2 position;
+
+void main() {
+	gl_Position = vec4(position, 0.0, 1.0);
+}
+)VSH";
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vsource, NULL);
+	glCompileShader(vertexShader);
+
+	// fragment shader
+	const char* fsource = R"FSH(
+#version 150
+
+out vec4 outColor;
+
+void main() {
+	outColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+)FSH";
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fsource, NULL);
+	glCompileShader(fragmentShader);
+
+	// combine shaders into a program
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+
+	// make link between vertex data and attributes
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(posAttrib);
+
 	// main loop
 	while(!glfwWindowShouldClose(window)) {
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	        glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
+
 		glfwPollEvents();
 	}
 
